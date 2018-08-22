2d244413f1abf7e7add195aed2969d179b693c23
arleighburke <42553294+arleighburke@users.noreply.github.com>
2018-08-22 13:50:28 +0200

    Update akatsuki.py

--- akatsuki.py
+++ akatsuki.py
@@ -30,6 +30,7 @@ if i == 0 :
 	print (' [2] nmap  ')
 	print (' [3] dmitry  ')
 	print (' [4] recon-ng  ')
+	print (' [5] back')
 	i1 = input ('Select Your Tool : ')
 	if i1 == 0 :
 		os.system('whois')
@@ -51,3 +52,59 @@ if i == 0 :
 		os.system('recon-ng')
 	else:
 		print ('Tools not install Try to install ,')
+	if i1 == 5 :
+		print ('================================================')
+		print ('')
+		print (' [0] Information Gathering ')
+		print (' [1] Vulnerability Analysis')
+		print (' [2] Web Application analysis  ')
+		print (' [3] Password attacks  ')
+		print (' [4] Wireless Attack  ')
+		print (' [5] sniffing & spoofing  ')
+		print (' [6] Social Engineering  ')
+		print (' [7] Exit  ')
+		print ('')
+	i = input ('Select Your choise : ')
+if i == 1 :
+	print ('================================================')
+	print (' [0] golismero ')
+	print (' [1] lynis ')
+	print (' [2] nikto  ')
+	print (' [3] nmap  ')
+	print (' [4] unix-privesc-check  ')
+	print (' [5] back')
+	i1 = input ('Select Your Tool : ')
+	if i1 == 0 :
+		os.system('golismero')
+	else:
+		print ('Tools not install Try to install ,')
+	if i1 == 1 :
+		os.system('lynis')
+	else:
+		print ('Tools not install Try to install ,')
+	if i1 == 2 :
+		os.system('nikto')
+	else:
+		print ('Tools not install Try to install ,')
+	if i1 == 3 :
+		os.system('nmap')
+	else:
+		print ('Tools not install Try to install ,')
+	if i1 == 4 :
+		os.system('unix-privesc-check')
+	else:
+		print ('Tools not install Try to install ,')
+	if i1 == 5 :
+		print ('================================================')
+		print ('')
+		print (' [0] Information Gathering ')
+		print (' [1] Vulnerability Analysis')
+		print (' [2] Web Application analysis  ')
+		print (' [3] Password attacks  ')
+		print (' [4] Wireless Attack  ')
+		print (' [5] sniffing & spoofing  ')
+		print (' [6] Social Engineering  ')
+		print (' [7] Exit  ')
+		print ('')
+	i = input ('Select Your choise : ')
+
